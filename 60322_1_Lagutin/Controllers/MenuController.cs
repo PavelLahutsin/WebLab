2862eb6207c50cc07d1abd918db0d394372150be
Pavel <pavelvasilevich@gmail.com>
2017-09-16 18:52:49 +0300

    lab3 5.6

---
+++ 60322_1_Lagutin/Controllers/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using _60322_1_Lagutin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _60322_1_Lagutin.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+
+        List<MenuItem> items;
+        public MenuController()
+        {
+            items = new List<MenuItem>
+            {
+                new MenuItem{Name="Домой", Controller="Home",
+                Action="Index", Active=string.Empty},
+                new MenuItem{Name="Каталог", Controller="Product",
+                Action="List", Active=string.Empty},
+                new MenuItem{Name="Администрирование", Controller="Admin",
+                Action="Index", Active=string.Empty},
+            };
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Main(string a = "Index", string c = "Home")
+        {
+            items.First(m => m.Controller == c).Active = "active";
+            return PartialView(items);
+        }
+        public PartialViewResult UserInfo()
+        {
+            return PartialView();
+        }
+        public string Side()
+        {
+            return "Боковая панель";
+        }
+        public PartialViewResult Map()
+        {
+            return PartialView(items);
+        }
+    }
+}
---
+++ 60322_1_Lagutin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_60322_1_Lagutin.MvcApplication" Language="C#" %>
